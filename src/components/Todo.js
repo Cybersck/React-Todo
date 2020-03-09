80456303c6cf9aab9a3cee479bd00cdaa4a4c925
kevin lautermilch <kevin.laut@outlook.com>
2020-03-09 16:08:25 -0400

    :)

---
+++ src/components/Todo.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {FormGroup, Label, Input} from 'reactstrap';
+import './Todo.css';
+
+class Todo extends React.Component {
+
+render() {
+    return (
+        <div>
+            <FormGroup check onChange={() => this.props.update(this.props.todo.id)}>
+                <Label check>
+                    <Input type="checkbox" />{' '}
+                    {this.props.todo.completed === false && this.props.todo.task}
+                    {this.props.todo.completed === true && <span className='completed'>{this.props.todo.task}</span>}
+                </Label>
+            </FormGroup>
+        </div>
+    )
+}
+}
+
+export default Todo;
